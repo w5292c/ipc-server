7f97b34a6c7b2ac30e35fe4c0f1cbf3520b70f28
Alexander Chumakov <w5292c@gmail.com>
2017-01-27 16:37:14 +0300

    Added preliminary implementation for handling IPC connections

--- library/ipc-channel.c
+++ library/ipc-channel.c
@@ -27,11 +27,23 @@
 #include "ipc-common.h"
 #include "ipc-logging.h"
 
+#include <errno.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <pthread.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define IPC_PORT_NUMBER (1070)
+#define IPC_POLLING_TIME_USEC (100000)
+#define IPC_CHANNEL_CONNECTIONS_NUMBER (4)
 
 typedef struct _IpcChannel {
-  pthread_t channelTId;
+  bool aliveFlag;
+  pthread_t channelThread;
 } IpcChannel;
 
 static void *ipc_channel_thread(void *arg);
@@ -39,6 +51,8 @@ static void *ipc_channel_thread(void *arg);
 IpcChannel *ipc_channel_new(void)
 {
   IpcChannel *const self = calloc(1, sizeof (IpcChannel));
+  self->aliveFlag = true;
+
   return self;
 }
 
@@ -50,16 +64,31 @@ void ipc_channel_free(IpcChannel *self)
 void ipc_channel_stop(IpcChannel *self)
 {
   IPC_LOG_DEBUG("Request to stop the IPC channel");
+  self->aliveFlag = false;
+  const int res = pthread_join(self->channelThread, NULL);
+  if (res) {
+    IPC_LOG_ERROR("Failed stopping the IPC thread, code: %d", res);
+    return;
+  }
+
+  IPC_LOG_DEBUG("Stopped the IPC thread");
 }
 
 void ipc_channel_start(IpcChannel *self)
 {
-  if (self->channelTId) {
+  if (self->channelThread) {
     IPC_LOG_ERROR("Already started");
     return;
   }
 
   IPC_LOG_DEBUG("Starting channel engine...");
+  const int res = pthread_create(&self->channelThread, NULL, ipc_channel_thread, self);
+  if (res) {
+    self->aliveFlag = false;
+    IPC_LOG_ERROR("Failed creating the IPC thread, code: %d", res);
+    return;
+  }
+
   IPC_LOG_DEBUG("Starting channel engine: done.");
 }
 
@@ -68,5 +97,52 @@ void *ipc_channel_thread(void *data)
   IpcChannel *const self = (IpcChannel *)data;
   IPC_UNUSED(self);
 
+  const int sockfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+  if (-1 == sockfd) {
+    self->aliveFlag = false;
+    IPC_LOG_ERROR("Failed creating socket for IPC, code: %d", errno);
+    return NULL;
+  }
+
+  struct sockaddr_in address;
+  memset(&address, 0, sizeof (address));
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(IPC_PORT_NUMBER);
+
+  int res = bind(sockfd, (const struct sockaddr *)&address, sizeof (address));
+  if (res) {
+    IPC_LOG_ERROR("Failed binding the IPC channel, code: %d", errno);
+    self->aliveFlag = false;
+    close(sockfd);
+    return NULL;
+  }
+
+  res = listen(sockfd, IPC_CHANNEL_CONNECTIONS_NUMBER);
+  if (res) {
+    IPC_LOG_ERROR("Failed starting listening, code: %d", errno);
+    self->aliveFlag = false;
+    close(sockfd);
+    return NULL;
+  }
+
+  while (self->aliveFlag) {
+    const int clientSocket = accept4(sockfd, NULL, NULL, 0);
+    if (-1 == clientSocket) {
+      if (EAGAIN != errno) {
+        IPC_LOG_ERROR("Failed accepting new connection, code: %d", errno);
+        break;
+      }
+    } else {
+      IPC_LOG_DEBUG("Got new connection, %d", clientSocket);
+      /** @todo Implement handling the new connection */
+      close(clientSocket);
+    }
+    usleep(IPC_POLLING_TIME_USEC);
+  }
+
+  /* Cleanup */
+  self->aliveFlag = false;
+  close(sockfd);
   return NULL;
 }
